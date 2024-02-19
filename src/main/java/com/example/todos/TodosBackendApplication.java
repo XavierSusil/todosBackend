8b7e5b96a51119956de68d3f54e4af756c5a9830
Susil A <xaviersusil@gmail.com>
2024-02-19 16:13:08 +0530

    Secret key put into environment variables

--- src/main/java/com/example/todos/TodosBackendApplication.java
+++ src/main/java/com/example/todos/TodosBackendApplication.java
@@ -1,23 +1,15 @@
 package com.example.todos;
 
-import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.todos.service.JwtService;
 
 @SpringBootApplication
 public class TodosBackendApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TodosBackendApplication.class, args);
-    printEnvironmentVariables();
+    
   }
   
-  public static void printEnvironmentVariables() {
-    Map<String,String> env = System.getenv();
-    
-    for (String key : env.keySet()) {
-      String value = env.get(key);
-      System.out.println(key + " = " + value);
-    }
-  }
 }
